8a5c37f85c3689b67d4c7f2f645cd0a7b898ec6c
Tony-Balony <39902616+Tony-Balony@users.noreply.github.com>
2026-08-05 16:35:06 +0000

    title

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import altair as alt
 # ------------------------------------------------------------
 
 st.set_page_config(
-    page_title="Eurostat Trade in Services Explorer",
+    page_title="Eurostat CrosBorder Trade in Services Explorer",
     layout="wide"
 )
 
